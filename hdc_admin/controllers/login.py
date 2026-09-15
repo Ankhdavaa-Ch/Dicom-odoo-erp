8d29123005de4488377f8c7587c96a971a1798c8
Ankhdavaa-Ch <144655463+Ankhdavaa-Ch@users.noreply.github.com>
2026-09-15 10:48:24 +0800

    Fix HDC login audit controller

--- hdc_admin/controllers/login.py
+++ hdc_admin/controllers/login.py
@@ -1,1 +1,30 @@
-x
+from odoo.http import request
+from odoo.addons.web.controllers.home import Home
+
+
+class HdcHome(Home):
+
+    def web_login(self, redirect=None, **kw):
+        was_logged_in = bool(request.session.uid)
+        response = super().web_login(redirect=redirect, **kw)
+
+        if (
+            request.httprequest.method == 'POST'
+            and not was_logged_in
+            and request.session.uid
+        ):
+            user = request.env['res.users'].sudo().browse(request.session.uid)
+            forwarded_for = request.httprequest.headers.get('X-Forwarded-For')
+            ip_address = (
+                forwarded_for.split(',')[0].strip()
+                if forwarded_for
+                else request.httprequest.remote_addr
+            )
+            request.env['hdc.login.log'].sudo().create({
+                'user_id': user.id,
+                'login': user.login,
+                'ip_address': ip_address,
+                'user_agent': request.httprequest.headers.get('User-Agent'),
+            })
+
+        return response
