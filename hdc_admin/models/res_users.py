7071cfff9092ff3e64981c9dd8ea63ba424559ea
Ankhdavaa-Ch <144655463+Ankhdavaa-Ch@users.noreply.github.com>
2026-09-16 11:22:59 +0800

    Link system users to employees

--- hdc_admin/models/res_users.py
+++ hdc_admin/models/res_users.py
@@ -4,6 +4,13 @@ from odoo import api, fields, models
 class ResUsers(models.Model):
     _inherit = 'res.users'
 
+    hdc_employee_id = fields.Many2one(
+        'hr.employee',
+        string='Ажилтан',
+        domain="[('user_id', 'in', [False, id])]",
+        help='Системийн хэрэглэгчтэй холбох ажилтан.',
+    )
+
     hdc_role_ids = fields.Many2many(
         'res.groups',
         string='Системийн дүр',
@@ -40,3 +47,33 @@ class ResUsers(models.Model):
             selected_roles = user.hdc_role_ids & role_groups
             other_groups = user.groups_id - role_groups
             user.groups_id = [(6, 0, (other_groups | selected_roles).ids)]
+
+    @api.onchange('hdc_employee_id')
+    def _onchange_hdc_employee_id(self):
+        for user in self:
+            if user.hdc_employee_id:
+                user.name = user.hdc_employee_id.name
+                if user.hdc_employee_id.work_email:
+                    user.email = user.hdc_employee_id.work_email
+                if user.hdc_employee_id.work_phone:
+                    user.phone = user.hdc_employee_id.work_phone
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        users = super().create(vals_list)
+        for user in users:
+            if user.hdc_employee_id and user.hdc_employee_id.user_id != user:
+                user.hdc_employee_id.sudo().user_id = user.id
+        return users
+
+    def write(self, vals):
+        old_employees = {user.id: user.hdc_employee_id for user in self}
+        result = super().write(vals)
+        if 'hdc_employee_id' in vals:
+            for user in self:
+                old_employee = old_employees.get(user.id)
+                if old_employee and old_employee != user.hdc_employee_id and old_employee.user_id == user:
+                    old_employee.sudo().user_id = False
+                if user.hdc_employee_id and user.hdc_employee_id.user_id != user:
+                    user.hdc_employee_id.sudo().user_id = user.id
+        return result
