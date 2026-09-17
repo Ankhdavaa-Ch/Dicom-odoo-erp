4a699673587f5f9981386c97a7ecd4bd2adc24c2
Ankhdavaa-Ch <144655463+Ankhdavaa-Ch@users.noreply.github.com>
2026-09-17 10:21:59 +0800

    Add employee service manifest

---
+++ hdc_employee_service/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    'name': 'HDC Employee Service',
+    'version': '18.0.1.0.0',
+    'summary': 'Ажилтны өөртөө үйлчлэх хэсэг',
+    'category': 'Human Resources',
+    'author': 'HDC',
+    'license': 'LGPL-3',
+    'depends': ['base', 'web', 'hr', 'hdc_attendance', 'hdc_erp_base'],
+    'data': [
+        'security/employee_service_security.xml',
+        'views/employee_service_menu.xml',
+    ],
+    'assets': {
+        'web.assets_backend': [
+            'hdc_employee_service/static/src/js/my_attendance.js',
+            'hdc_employee_service/static/src/xml/my_attendance.xml',
+            'hdc_employee_service/static/src/scss/my_attendance.scss',
+        ],
+    },
+    'installable': True,
+    'application': False,
+}
