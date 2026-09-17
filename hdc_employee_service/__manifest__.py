dac80d68b8281018bb9f80b2df821ca356db842f
Ankhdavaa-Ch <144655463+Ankhdavaa-Ch@users.noreply.github.com>
2026-09-17 15:25:43 +0800

    Load employee self service profile view

--- hdc_employee_service/__manifest__.py
+++ hdc_employee_service/__manifest__.py
@@ -1,13 +1,14 @@
 {
     'name': 'HDC Employee Service',
-    'version': '18.0.1.0.0',
+    'version': '18.0.1.1.0',
     'summary': 'Ажилтны өөртөө үйлчлэх хэсэг',
     'category': 'Human Resources',
     'author': 'HDC',
     'license': 'LGPL-3',
-    'depends': ['base', 'web', 'hr', 'hdc_attendance', 'hdc_erp_base'],
+    'depends': ['base', 'web', 'hr', 'hdc_hr', 'hdc_attendance', 'hdc_erp_base'],
     'data': [
         'security/employee_service_security.xml',
+        'views/my_profile_views.xml',
         'views/employee_service_menu.xml',
     ],
     'assets': {
