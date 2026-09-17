a266148466d1c382d1623f86e4f209f4ef959a90
Ankhdavaa-Ch <144655463+Ankhdavaa-Ch@users.noreply.github.com>
2026-09-17 16:11:38 +0800

    Load employee request workflow data and views

--- hdc_employee_service/__manifest__.py
+++ hdc_employee_service/__manifest__.py
@@ -1,14 +1,19 @@
 {
     'name': 'HDC Employee Service',
-    'version': '18.0.1.1.0',
+    'version': '18.0.1.2.0',
     'summary': 'Ажилтны өөртөө үйлчлэх хэсэг',
     'category': 'Human Resources',
     'author': 'HDC',
     'license': 'LGPL-3',
-    'depends': ['base', 'web', 'hr', 'hdc_hr', 'hdc_attendance', 'hdc_erp_base'],
+    'depends': ['base', 'web', 'hr', 'mail', 'hdc_hr', 'hdc_attendance', 'hdc_erp_base'],
     'data': [
         'security/employee_service_security.xml',
+        'security/ir.model.access.csv',
+        'security/employee_request_rules.xml',
+        'data/employee_request_sequence.xml',
         'views/my_profile_views.xml',
+        'views/hr_department_views.xml',
+        'views/employee_request_views.xml',
         'views/employee_service_menu.xml',
     ],
     'assets': {
