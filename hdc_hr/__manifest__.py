8cb73412b017c9322ec3774e6092808e9ea4a99e
Ankhdavaa-Ch <144655463+Ankhdavaa-Ch@users.noreply.github.com>
2026-09-21 14:36:33 +0800

    Load organization chart styling

--- hdc_hr/__manifest__.py
+++ hdc_hr/__manifest__.py
@@ -5,29 +5,20 @@
     'category': 'Human Resources',
     'author': 'HDC',
     'license': 'LGPL-3',
-
-    'depends': [
-        'hr',
-        'mail',
-    ],
-
+    'depends': ['hr', 'mail'],
     'data': [
         'security/ir.model.access.csv',
-
         'views/hr_employee_views.xml',
-
         'views/hr_structure_views.xml',
         'views/hr_structure_node_views.xml',
-
-        # ЭХЛЭЭД action/view
         'views/hr_job_views.xml',
-
-        # ХАМГИЙН СҮҮЛД menu
         'views/hr_structure_menu.xml',
-
-        
     ],
-
+    'assets': {
+        'web.assets_backend': [
+            'hdc_hr/static/src/scss/organization_chart.scss',
+        ],
+    },
     'installable': True,
     'application': True,
 }
